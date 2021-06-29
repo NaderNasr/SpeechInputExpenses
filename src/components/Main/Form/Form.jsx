c2167b4fc4a68bb9ee267f4a57b30ed72c8a488f
NADER NASR <nadernasr7@gmail.com>
2021-06-29 14:47:32 -0400

    change log upper to lowercase

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -3,8 +3,8 @@ import { TextField, Typography, Grid, Button, FormControl, InputLabel, Select, M
 import { ExpenseTrackerContext } from '../../../context/context';
 import { v4 as uuidv4 } from 'uuid';
 import { incomeCategories, expenseCategories } from '../../../constants/categories';
-import formateDate from '../../../utils/formateDate'
-import {useSpeechContext} from '@speechly/react-client'
+import formateDate from '../../../utils/formateDate';
+import { useSpeechContext } from '@speechly/react-client';
 
 import useStyles from './styles';
 
@@ -15,15 +15,12 @@ const initialState = {
 	date: formateDate(new Date())
 };
 
-
-
-
 const Form = () => {
 	const classes = useStyles();
 	const [ formData, setFormData ] = useState(initialState);
 	// console.log(formData);
 	const { addTrans } = useContext(ExpenseTrackerContext);
-	const {segment} = useSpeechContext()
+	const { segment } = useSpeechContext();
 
 	const createTrans = () => {
 		const trans = { ...formData, amount: Number(formData.amount), id: uuidv4() };
@@ -31,33 +28,49 @@ const Form = () => {
 		setFormData(initialState);
 	};
 
-	useEffect(() => {
-		if(segment){
-			if(segment.intent.intent === 'add_expense'){
-				setFormData({...formData, type: 'Expense'})
-			} else if(segment.intent.intent === 'add_income'){
-				setFormData({...formData, type: 'Income'})
-			} else if(segment.isFinal && segment.intent.intent === "create_transaction") {
-				return createTrans()
-			} else if (segment.isFinal && segment.intent.intent === "cancel_transaction") {
-				return setFormData(initialState)
+	useEffect(
+		() => {
+			if (segment) {
+				if (segment.intent.intent === 'add_expense') {
+					setFormData({ ...formData, type: 'Expense' });
+				} else if (segment.intent.intent === 'add_income') {
+					setFormData({ ...formData, type: 'Income' });
+				} else if (segment.isFinal && segment.intent.intent === 'create_transaction') {
+					return createTrans();
+				} else if (segment.isFinal && segment.intent.intent === 'cancel_transaction') {
+					return setFormData(initialState);
+				}
+
+				segment.entities.forEach((e) => {
+					// console.log(e.value)
+					const category = `${e.value.charAt(0)}${e.value.slice(1).toLowerCase()}`
+					switch (e.type) {
+						case 'amount':
+							setFormData({ ...formData, amount: e.value });
+							break;
+						case 'category':
+							setFormData({ ...formData, category });
+							break;
+						case 'date':
+							setFormData({ ...formData, date: e.value });
+							break;
+
+						default:
+							break;
+					}
+				});
 			}
+		},
+		[ segment ]
+	);
 
-			segment.entities.forEach((e) => {
-				console.log(e.value)
-			})
-		}
-	}, [segment])
-
-    const selectedCategories = formData.type === 'Income' ? incomeCategories  : expenseCategories
+	const selectedCategories = formData.type === 'Income' ? incomeCategories : expenseCategories;
 
 	return (
 		<Grid container spacing={2}>
 			<Grid item xs={12}>
 				<Typography variant="subtitle2" align="center" gutterBottom>
-					{segment && segment.words.map((w) => w.value).join(" ")}
-						
-					
+					{segment && segment.words.map((w) => w.value).join(' ')}
 				</Typography>
 			</Grid>
 			<Grid item x={6}>
@@ -76,7 +89,11 @@ const Form = () => {
 						value={formData.category}
 						onChange={(e) => setFormData({ ...formData, category: e.target.value })}
 					>
-						{selectedCategories.map((cat) => <MenuItem key={cat.type} value={cat.type}>{cat.type}</MenuItem>)}
+						{selectedCategories.map((cat) => (
+							<MenuItem key={cat.type} value={cat.type}>
+								{cat.type}
+							</MenuItem>
+						))}
 					</Select>
 				</FormControl>
 			</Grid>
